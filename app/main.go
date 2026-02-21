3346ba3ba91ad387973910fa4718425de767bf30
adriankopytko <15912513+adriankopytko@users.noreply.github.com>
2026-02-21 21:58:29 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -69,27 +69,29 @@ func main() {
 		panic("No choices in response")
 	}
 
-	tool_call := resp.Choices[0].Message.ToolCalls[0]
+	if len(resp.Choices[0].Message.ToolCalls) > 0 {
+		tool_call := resp.Choices[0].Message.ToolCalls[0]
 
-	tool_name := tool_call.Function.Name
+		tool_name := tool_call.Function.Name
 
-	if tool_name == "Read" {
-		var args map[string]interface{}
+		if tool_name == "Read" {
+			var args map[string]interface{}
 
-		err := json.Unmarshal([]byte(tool_call.Function.Arguments), &args)
+			err := json.Unmarshal([]byte(tool_call.Function.Arguments), &args)
 
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "error parsing arguments: %v\n", err)
-			os.Exit(1)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "error parsing arguments: %v\n", err)
+				os.Exit(1)
+			}
+
+			content, err := os.ReadFile(args["file_path"].(string))
+
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "error reading file: %v\n", err)
+				os.Exit(1)
+			}
+			fmt.Print(string(content))
 		}
-
-		content, err := os.ReadFile(args["file_path"].(string))
-
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "error reading file: %v\n", err)
-			os.Exit(1)
-		}
-		fmt.Print(string(content))
 	}
 
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
